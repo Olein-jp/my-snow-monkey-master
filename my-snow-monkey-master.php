01596dbde10f5c440c4d4005cd0d1770ab74d027
Olein-jp <koji.kuno@gmail.com>
2022-12-27 08:22:44 +0900

    :white_check_mark: 定数名の変更

--- my-snow-monkey-master.php
+++ my-snow-monkey-master.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin name: My Snow Monkey
- * Description: Olein Design 制作の My Snow Monkey
+ * Plugin name: My Snow Monkey Master
+ * Description: Snow Monkey をカスタマイズするための最強雛形（を目指しています）
  * Version: 1.0.0
  *
- * @package my-snow-monkey
- * @author inc2734
+ * @package my-snow-monkey-master
+ * @author Olein-jp
  * @license GPL-2.0+
  */
 
@@ -22,14 +22,14 @@ if ( 'snow-monkey' !== $theme->template && 'snow-monkey/resources' !== $theme->t
  *
  * @var string
  */
-define( 'MY_SNOW_MONKEY_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+define( 'MY_SNOW_MONKEY_MASTER_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 
 /**
  * Directory path of this plugin
  *
  * @var string
  */
-define( 'MY_SNOW_MONKEY_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'MY_SNOW_MONKEY_MASTER_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 
 /**
  * Register style
@@ -39,16 +39,16 @@ add_action(
 	function () {
 		wp_enqueue_style(
 			'my-snow-monkey-style',
-			MY_SNOW_MONKEY_URL . '/build/css/style.css',
+			MY_SNOW_MONKEY_MASTER_URL . '/build/css/style.css',
 			array( Framework\Helper::get_main_style_handle() ),
-			filemtime( MY_SNOW_MONKEY_PATH . '/build/css/style.css' )
+			filemtime( MY_SNOW_MONKEY_MASTER_PATH . '/build/css/style.css' )
 		);
 
 		wp_enqueue_script(
 			'my-snow-monkey-scripts',
-			MY_SNOW_MONKEY_URL . '/build/js/scripts.js',
+			MY_SNOW_MONKEY_MASTER_URL . '/build/js/scripts.js',
 			null,
-			filemtime( MY_SNOW_MONKEY_PATH . '/build/js/scripts.js' ),
+			filemtime( MY_SNOW_MONKEY_MASTER_PATH . '/build/js/scripts.js' ),
 			true
 		);
 	}
@@ -62,6 +62,6 @@ add_action(
 	function () {
 		add_theme_support( 'editor-styles' );
 
-		add_editor_style( MY_SNOW_MONKEY_URL . '/build/css/editor-style.css' );
+		add_editor_style( MY_SNOW_MONKEY_MASTER_URL . '/build/css/editor-style.css' );
 	}
 );
